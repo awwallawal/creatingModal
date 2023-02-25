2ea720a12e6ac47853b1fbe722e52f971c4dcc88
awwallawal <103115087+awwallawal@users.noreply.github.com>
2023-02-25 09:39:30 +0100

    Initial Commit
    Initial Commit of Files

---
+++ script.js
@@ -0,0 +1,12 @@
+const openModal = document.getElementById('open-modal');
+const modal = document.getElementById('modal');
+const overlay = document.getElementById('overlay');
+const closeModal = document.getElementById('close-modal');
+
+openModal.addEventListener('click', function(){
+ overlay.style.display = 'block';
+});
+
+closeModal.addEventListener('click', function(){
+ overlay.style.display = 'none';
+})
